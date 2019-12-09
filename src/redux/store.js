60b3101589251d0adfc08770deffafea70ce276a
Rick Pearce <rpearce63@yahoo.com>
2019-12-08 18:52:03 -0600

    removing redux-logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,12 @@ import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [];
+
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
