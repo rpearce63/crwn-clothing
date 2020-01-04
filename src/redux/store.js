a05480bbb6ea13fb35aa232f95a70d83e33f313c
Rick Pearce <rpearce63@yahoo.com>
2020-01-04 09:12:14 -0600

    refactor to use Container Pattern for spinner when loading

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware } from "redux";
 
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-
+import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-const middlewares = [];
+const middlewares = [thunk];
 
 // eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === "development") {
